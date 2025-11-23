54d540d8b213ea44f50fd7fce62e816b00c5ed19
Amit <amittupe1000@gmail.com>
2025-11-23 20:32:05 +0530

    Added Login and Register Functionality

--- django_chatbot/chatbot/urls.py
+++ django_chatbot/chatbot/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns=[
-    path('',views.chatbot,name='chatbot')
+    path('',views.chatbot,name='chatbot'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('register/',views.register,name='register'),
 ]
